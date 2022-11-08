216b653b6662f9f60199897766d9f1c3370472b8
Nguyen Manh Thang <thangnm@bglobalcorp.com>
2022-11-08 19:38:49 +0700

    update header and footer

--- components/_header.php
+++ components/_header.php
@@ -1,21 +1,24 @@
 <?php
-$isLogin = true;
+$isLogin = false;
 $isAdmin = false;
 
 ?>
 
 <div class="header">
-    <a href="/" class="logo">Food Blog</a>
-    <div class="header-right">
-        <?php if ($isLogin) : ?>
-            <a href="javascript:void(0)" id="header-post">Đăng bài</a>
-            <?php if ($isAdmin) : ?>
-                <a href="javascript:void(0)" id="header-post-management">Quản lý bài viết</a>
+    <div class="container container-flex align-items-center justify-content-between">
+        <a href="/" class="logo">Food Blog</a>
+        <div class="header-content">
+            <a href="/" id="header-post" class="<?php echo uri() === '/' ? 'nav-active' : '' ?>">Trang chủ</a>
+            <?php if ($isLogin) : ?>
+                <a href="/posts/create" id="header-post">Đăng bài</a>
+                <?php if ($isAdmin) : ?>
+                    <a href="/admin/check" id="header-post-management">Quản lý bài viết</a>
+                <?php endif; ?>
+                <a href="javascript:void(0)" id="header-logout">Đăng xuất</a>
+            <?php else : ?>
+                <a href="javascript:void(0)" id="header-register">Đăng ký</a>
+                <a href="javascript:void(0)" id="header-login">Đăng nhập</a>
             <?php endif; ?>
-            <a href="javascript:void(0)" id="header-logout">Đăng xuất</a>
-        <?php else : ?>
-            <a href="javascript:void(0)" id="header-register">Đăng ký</a>
-            <a href="javascript:void(0)" id="header-login">Đăng nhập</a>
-        <?php endif; ?>
+        </div>
     </div>
 </div>
